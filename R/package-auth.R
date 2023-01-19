356ba93ec7244a343f74e0adf4246577225fb8f2
skodman <skodman@gmail.com>
2023-01-19 16:24:01 +0800

    Preliminary support for multi-profile workflows
    Co-authored-by: Nenad Nikolic <Nenad.Nikolic@lotterywest.wa.gov.au>

--- R/package-auth.R
+++ R/package-auth.R
@@ -16,9 +16,10 @@
 #'
 #' @return workspace URL
 #' @export
-db_host <- function(id = NULL, prefix = NULL) {
+db_host <- function(id = NULL, prefix = NULL,env=getOption("db_env")) {
   if (is.null(id) && is.null(prefix)) {
-    host <- Sys.getenv("DATABRICKS_HOST")
+    #host <- Sys.getenv("DATABRICKS_HOST")
+    host=db_get_param('dbHost',env)
 
     if (host == "") {
       stop(format_error(c(
@@ -45,9 +46,9 @@ db_host <- function(id = NULL, prefix = NULL) {
 #' @return databricks token
 #' @import cli
 #' @export
-db_token <- function() {
-  token <- Sys.getenv("DATABRICKS_TOKEN")
-
+db_token <- function(env=getOption("db_env")) {
+  #token <- Sys.getenv("DATABRICKS_TOKEN")
+  token=db_get_param('dbToken',env)
   if (token == "") {
     stop(cli::format_error(c(
       "`DATABRICKS_TOKEN` not found in `.Renviron`:",
@@ -71,17 +72,18 @@ db_token <- function() {
 #' @return databricks workspace ID
 #' @import cli
 #' @export
-db_wsid <- function() {
-  token <- Sys.getenv("DATABRICKS_WSID")
+db_wsid <- function(env=getOption("db_env")) {
+  #token <- Sys.getenv("DATABRICKS_WSID")
+  wsid=db_get_param('dbWsId',env)
 
-  if (token == "") {
+  if (wsid == "") {
     stop(cli::format_error(c(
       "`DATABRICKS_WSID` not found in `.Renviron`:",
       "x" = "Need to specify `DATABRICKS_WSID` within `.Renviron` file."
     )))
   }
 
-  token
+  wsid
 }
 
 
@@ -106,4 +108,74 @@ db_read_netrc <- function(path = "~/.netrc") {
 #' @param token Databricks workspace token, defaults to calling [db_token()].
 #'
 NULL
+
+
+
+#' Parses .databrickscfg file for parameters related to brickster
+#' Uses db_env variable that is set with options(db_env,...)
+#' @param lParm name of the parameter to extract from cfg file. Default value is DB workspace ID (dbWsId)
+#'
+#' @return
+#' @export
+#'
+#' @examples
+db_get_param=function(lParm='dbWsId',lEnv=NULL){
+  # browser()
+  if(is.null(getOption("use_databrickscfg"))){
+    #Read .Renviron
+    parms=list(dbWsId="DATABRICKS_WSID",
+               dbToken="DATABRICKS_TOKEN",
+               dbHost="DATABRICKS_HOST")
+
+
+    lVar=paste0(parms[lParm],ifelse(is.null(lEnv),"",paste0("_",stringr::str_to_upper(lEnv))))
+
+    lVal=Sys.getenv(lVar)
+    if(lVal=="") {
+      stop(cli::format_error(c(
+        paste0("Parameter {.var ",lVar,"} does not exist in {.var .Renviron} file:"),
+        "x" = paste0("Need to specify {.var ",lVar,"} within {.var .Renviron} file.")
+      )))
+    }
+
+
+
+  }else{
+    # Read .databrickscfg
+
+
+    lHome=ifelse(Sys.info()[[1]]=="Windows",Sys.getenv("USERPROFILE"),Sys.getenv("HOME"))
+    con=base::file(file.path(lHome,".databrickscfg"),"r",blocking=F)
+    fileLines=base::readLines(con)
+    close(con)
+    fileLines=fileLines[stringr::str_trim(fileLines)!=""]
+
+    parms=list(dbWsId="DATABRICKS_WSID",
+               dbToken="token",
+               dbHost="host")
+    lDbEnv=ifelse(is.null(lEnv),"DEFAULT",lEnv)
+    # lParm='DATABRICKS_HOST'
+
+    sections=c(base::which(stringr::str_detect(fileLines,'^\\[.{1,}\\]$')),base::length(fileLines))
+    envLine=base::which(stringr::str_detect(fileLines[sections],lDbEnv))
+    if (purrr::is_empty(envLine)) {
+      stop(cli::format_error(c(
+        paste0("{.var ",lDbEnv,"} profile not found in `.databrickscfg`:"),
+        "x" = paste0("Need to specify {.var ",lDbEnv,"} profile within `.databrickscfg` file.")
+      )))
+    }
+
+    lLines=fileLines[(sections[envLine]):(sections[envLine+1])]
+    prmLine=lLines[which(stringr::str_detect(lLines,parms[[lParm]]))]
+    if (purrr::is_empty(prmLine)) {
+      stop(cli::format_error(c(
+        paste0("Parameter {.var ",parms[lParm],"} does not exist in {.var ",lDbEnv,"} workspace section of `.databrickscfg` file:"),
+        "x" = paste0("Need to specify {.var ",parms[lParm],"} within {.var ",lDbEnv,"} workspace section.")
+      )))
+    }
+
+    lVal=stringr::str_trim((base::strsplit(prmLine,"=")[[1]])[2])
+  }
+  lVal
+}
 
