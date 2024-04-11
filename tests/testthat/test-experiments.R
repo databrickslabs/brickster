2f7030d5af425721d83d93e0b81f5b54dbdcd7bc
Zac Davies <zac@databricks.com>
2024-04-11 22:01:47 +1000

    repos/query-hist/exps/feature-store (24.08%)

---
+++ tests/testthat/test-experiments.R
@@ -0,0 +1,39 @@
+test_that("experiments API", {
+
+  resp_list <- db_experiments_list(max_results = 1)
+  expect_type(resp_list, "list")
+  expect_named(resp_list[[1]])
+
+  resp_get_by_name <- db_experiments_get(name = resp_list[[1]]$name)
+  expect_equal(resp_list[[1]]$name, resp_get_by_name$name)
+
+  resp_get_by_id <- db_experiments_get(id = resp_list[[1]]$experiment_id)
+  expect_equal(resp_list[[1]]$experiment_id, resp_get_by_name$experiment_id)
+
+  # don't allow both
+  expect_error(db_experiments_get(
+    id = resp_list[[1]]$experiment_id,
+    name = resp_list[[1]]$name
+  ))
+
+})
+
+test_that("experiments API - don't perform", {
+
+  resp_list <- db_experiments_list(max_results = 1, perform_request = FALSE)
+  expect_s3_class(resp_list, "httr2_request")
+
+  resp_get_by_name <- db_experiments_get(
+    name = "some name",
+    perform_request = FALSE
+  )
+  expect_s3_class(resp_get_by_name, "httr2_request")
+
+  resp_get_by_id <- db_experiments_get(
+    id = "some id",
+    perform_request = FALSE
+  )
+  expect_s3_class(resp_get_by_id, "httr2_request")
+
+
+})
--- tests/testthat/test-repos.R
+++ tests/testthat/test-repos.R
@@ -1,28 +1,42 @@
-# http_mock_status <- function(status) {
-#   expr <- substitute(
-#     expression(
-#       function(x) {
-#         httr2::response(status_code = status)
-#       }
-#     ), list(status = status))
-#   eval(expr, envir = NULL)
-# }
-#
-# a <- http_mock_status(200)
-# a
-#
-# httr2::with_mock(my_mock, {
-#   db_repo_get(repo_id = 1, perform_request = FALSE) %>%
-#     httr2::req_perform()
-# })
-#
-#
-# test_that("repos api behaviour", {
-#
-#   expect_error()
-#   expect_s3_class()
-#   expect_
-#
-#
-#
-# })
+test_that("Feature Store API", {
+
+
+
+
+})
+
+
+test_that("Feature Store API - don't perform", {
+
+  resp_get_all <- db_repo_get_all("/", perform_request = FALSE)
+  expect_s3_class(resp_get_all, "httr2_request")
+
+  resp_create <- db_repo_create(
+    url = "some_url",
+    provider = "some_provider",
+    path = "some_path",
+    perform_request = FALSE
+  )
+  expect_s3_class(resp_create, "httr2_request")
+
+  resp_get <- db_repo_get(
+    repo_id = "some_id",
+    perform_request = FALSE
+  )
+  expect_s3_class(resp_get, "httr2_request")
+
+  resp_update <- db_repo_update(
+    repo_id = "some_id",
+    branch = "some_branch",
+    perform_request = FALSE
+  )
+  expect_s3_class(resp_get, "httr2_request")
+
+  resp_delete <- db_repo_delete(
+    repo_id = "some_id",
+    perform_request = FALSE
+  )
+  expect_s3_class(resp_get, "httr2_request")
+
+
+})
