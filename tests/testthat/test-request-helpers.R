40fb787deea3aefe86f95da3b27f48ab9f4a45d8
Zac Davies <zachary.davies+data@databricks.com>
2025-07-21 13:15:50 +1000

    Trying to fix failing test on CRAN R CMD Check that can't be repro'd locally.

--- tests/testthat/test-request-helpers.R
+++ tests/testthat/test-request-helpers.R
@@ -1,7 +1,6 @@
 test_that("request helpers - building requests", {
-
   host <- "some_url"
-  token <-  "some_token"
+  token <- "some_token"
   endpoint <- "clusters/create"
   endpoint_version <- "2.0"
   method <- "POST"
@@ -27,7 +26,7 @@ test_that("request helpers - building requests", {
   expect_identical(req$body$data, body)
   expect_no_error(req$options$useragent)
   expect_equal(req$policies$retry_max_tries, 3)
-  if (packageVersion("httr2") >= "1.1.2.9000") {
+  if (packageVersion("httr2") >= "1.2.0") {
     headers <- httr2::req_get_headers(req, "reveal")
   } else {
     headers <- req$headers
@@ -38,5 +37,4 @@ test_that("request helpers - building requests", {
   expect_equal(unclass(req_json), "{\"a\":1,\"b\":2}")
   expect_s3_class(req_json, "json")
   expect_null(db_request_json(NULL))
-
 })
